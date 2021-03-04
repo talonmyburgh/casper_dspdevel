d77497650dd3d607cedd49915e77679cd45929da
talonmyburgh <myburgh.talon@gmail.com>
2021-03-04 06:50:00 -0800

    Further developed coef gen

--- casper_filter/filtercoef_mem_gen.m
+++ casper_filter/filtercoef_mem_gen.m
@@ -1,13 +1,16 @@
-function coefs = coefgen(N, taps, win, fwidth)
+function coefs = filtercoef_mem_gen(N, taps, win, fwidth)
 %coefgen - Generate filterbank coeficients CASPER styles.
 %
 % Syntax: coefs = coefgen(N, taps, )
-%
-% Long description
-    w_str = {"hanning", "hamming", "bartlett", "blackman"};
-    f_handles = {@hanning, @hanning, @bartlett, @blackman}
-    WinDic = containers.Map(w_str, f_handles);
     alltaps = N*taps;
-    windowval = WinDic[win](alltaps);
-    coefs = (windowval .* sinc(fwidth .* (arange(0:alltaps-1)./N .- taps/2)));
+    if win == 'hanning'
+        windowval = transpose(hanning(alltaps));
+    elseif win == 'hamming'
+        windowval = transpose(hamming(alltaps));
+    elseif win == 'bartlett'
+        windowval = transpose(bartlett(alltaps));
+    elseif win == 'blackman'
+        windowval = transpose(blackman(alltaps));
+    end
+    coefs = windowval .* sinc(fwidth * ([0:alltaps-1]/N - taps/2));
 end
