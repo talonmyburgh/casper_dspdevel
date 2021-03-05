e6fbde572ecc1b0bc23359311652742b1294bd68
talonmyburgh <myburgh.talon@gmail.com>
2021-03-05 05:44:57 -0800

    Hex mem file generation working.

--- casper_filter/filtercoef_mem_gen.m
+++ casper_filter/filtercoef_mem_gen.m
@@ -1,16 +1,30 @@
-function coefs = filtercoef_mem_gen(N, taps, win, fwidth)
+function filtercoef_mem_gen(N,taps,win,fwidth,sign,b_width,f_width)
+    fixarray = fi(coefcalc(N,taps,win,fwidth),sign,b_width,f_width);
+    memfile = sprintf("filter_coefs_%d_%d_%s_%.2f.mem",N,taps,win,fwidth);
+    Mfile = fopen(memfile,'w');
+    if(Mfile == -1)
+        error("Cannot open mem file");
+    end
+    for i = 1:length(fixarray)
+       fprintf(Mfile,'%s\n',hex(fixarray(i))); 
+    end
+    fclose(Mfile);
+end
+function coefs = coefcalc(N, taps, win, fwidth)
 %coefgen - Generate filterbank coeficients CASPER styles.
 %
 % Syntax: coefs = coefgen(N, taps, )
     alltaps = N*taps;
-    if win == 'hanning'
+    if strcmp(win,'hanning')
         windowval = transpose(hanning(alltaps));
-    elseif win == 'hamming'
+    elseif strcmp(win,'hamming')
         windowval = transpose(hamming(alltaps));
-    elseif win == 'bartlett'
+    elseif strcmp(win,'bartlett')
         windowval = transpose(bartlett(alltaps));
-    elseif win == 'blackman'
+    elseif strcmp(win,'blackman')
         windowval = transpose(blackman(alltaps));
+    else
+        error("No suppported window function supplied to win variable");
     end
     coefs = windowval .* sinc(fwidth * ([0:alltaps-1]/N - taps/2));
 end
