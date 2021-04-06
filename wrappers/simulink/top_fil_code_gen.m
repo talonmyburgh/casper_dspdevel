23d64089a23596434ef78378cc3f953c7d0903a8
Talon Myburgh <myburgh.talon@gmail.com>
2021-04-06 06:34:45 -0700

    Fixed library to include PFB FIR

--- wrappers/simulink/top_fil_code_gen.m
+++ wrappers/simulink/top_fil_code_gen.m
@@ -72,12 +72,12 @@ function memfiles = top_fil_code_gen(wb_factor, nof_bands, nof_taps, win, fwidth
 
     %Generate coefficients mem file for the filter:
     command = sprintf("python3 -o %s -t %d -p %d -w %d -c %d -v %d -W %s -F %d -V %s",filepath, nof_taps, nof_bands, wb_factor, coef_dat_w, vendor, win, fwidth, "False")';
-    [status,cmdout] = system(command); %coefficient files will be generated at filepath/hex/
+    [~,cmdout] = system(command); %coefficient files will be generated at filepath/hex/
 
     %Update fil_pkg.vhd:
-    updatepkg(filepathscript, in_dat_w, out_dat_w, coef_dat_w, coef_dat_file, cmdout)
+    updatepkg(filepathscript, in_dat_w, out_dat_w, coef_dat_w, cmdout);
 
-    memfiles = [filepath cmdout]
+    memfiles = [filepath cmdout];
 end
 
 function chararr = mknprts(wb_factor)
