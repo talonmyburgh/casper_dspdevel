ed38296474da6d9c2300154556cbee1631bcfb2f
Talon Myburgh <myburgh.talon@gmail.com>
2021-10-29 20:56:54 +0200

    ** Neatening and cleaning

--- casper_adder/tb_tb_vu_common_add_sub.vhd
+++ casper_adder/tb_tb_vu_common_add_sub.vhd
@@ -1,24 +1,4 @@
--------------------------------------------------------------------------------
---
--- Copyright 2020
--- ASTRON (Netherlands Institute for Radio Astronomy) <http://www.astron.nl/>
--- P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
--- 
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
--- 
---     http://www.apache.org/licenses/LICENSE-2.0
--- 
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
---
--------------------------------------------------------------------------------
-
-LIBRARY IEEE, common_pkg_lib, common_components_lib, vunit_lib;
+LIBRARY IEEE, common_pkg_lib, vunit_lib;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 USE common_pkg_lib.common_pkg.ALL;
