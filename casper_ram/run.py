270a3b215519bd8797cccd8d6bacf233456f4a1d
talonmyburgh <myburgh.talon@gmail.com>
2021-11-03 19:03:51 +0200

    ^+ casper ram regression testing. Not working yet.

--- casper_ram/run.py
+++ casper_ram/run.py
@@ -1,21 +1,40 @@
 from vunit import VUnit
-import glob
+import os
 
 # Create VUnit instance by parsing command line arguments
+
+script_dir, _ = os.path.split(os.path.realpath(__file__))
 vu = VUnit.from_argv()
+
+lib = vu.add_library("xpm")
+lib.add_source_files("/mnt/c/Users/mybur/Work/xpm_vhdl/src/xpm/xpm_VCOMP.vhd")
+lib.add_source_files("/mnt/c/Users/mybur/Work/xpm_vhdl/src/xpm/xpm_memory/hdl/*.vhd")
+
+lib0 = vu.add_library("altera_mf")
+lib0.add_source_files(os.path.join(script_dir,"../altera/*.vhd"))
+
 lib1 = vu.add_library("casper_ram_lib")
-txt = glob.glob("*.vhd")
-for x in txt:
-    s = x.split('/')[-1]
-    if(s != "ip_sdp_ram_infer_tb.vhd" and s!="ip_tdp_ram_infer_tb.vhd" and s!= "tech_memory_ram_crwk_crw.vhd" and s!= "common_ram_crw_crw_ratio.vhd"
-    and s!= "altera_mf.vhd" and s!= "altera_mf_components.vhd" and s!= "ip_stratixiv_ram_cr_cw.vhd" and s!= "ip_stratixiv_ram_crw_crw.vhd" and s!= "ip_stratixiv_ram_crwk_crw.vhd"):
-        lib1.add_source_files(s)
+lib1.add_source_files(os.path.join(script_dir,"./*.vhd"))
+
+TB_GENERATED = lib1.test_bench("tb_tb_vu_common_paged_ram_crw_crw")
+TB_GENERATED.add_config(name = "common_paged_ram_crw_crw_test")
 
 lib2 = vu.add_library("common_components_lib")
-lib2.add_source_files("../common_components/*.vhd")
+lib2.add_source_files(os.path.join(script_dir,"../common_components/*.vhd"))
 
 lib3 = vu.add_library("common_pkg_lib")
-lib3.add_source_files("../common_pkg/*.vhd")
+lib3.add_source_files(os.path.join(script_dir,"../common_pkg/*.vhd"))
 
-# Run vunit function
+lib4 = vu.add_library("ip_xpm_ram_lib")
+lib4.add_source_files(os.path.join(script_dir,"../ip_xpm/ram/*.vhd"))
+
+lib5 = vu.add_library("ip_stratixiv_ram_lib")
+lib5.add_source_files(os.path.join(script_dir,"../ip_stratixiv/ram/*.vhd"))
+
+lib6 = vu.add_library("technology_lib",allow_duplicate = True)
+lib6.add_source_files(script_dir + "/../technology/technology_select_pkg.vhd")
+
+
+vu.set_compile_option("ghdl.a_flags", ["-frelaxed","-fsynopsys","-fexplicit"])
+vu.set_sim_option("ghdl.elab_flags", ["-frelaxed","-fsynopsys","-fexplicit","--syn-binding"])
 vu.main()
---
+++ casper_ram/tb_tb_vu_common_paged_ram_crw_crw.vhd
@@ -0,0 +1,48 @@
+LIBRARY IEEE, common_pkg_lib, vunit_lib;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+USE common_pkg_lib.common_pkg.ALL;
+context vunit_lib.vunit_context;
+
+entity tb_tb_vu_common_paged_ram_crw_crw is
+    GENERIC(
+    runner_cfg : string
+    );
+end tb_tb_vu_common_paged_ram_crw_crw;
+
+architecture tb of tb_tb_vu_common_paged_ram_crw_crw is
+
+    SIGNAL rst      	: STD_LOGIC;
+	SIGNAL clk      	: STD_LOGIC;
+	SIGNAL tb_end   	: STD_LOGIC;
+	SIGNAL test_msg   : STRING(1 to 50);
+	SIGNAL test_pass	: BOOLEAN;
+
+BEGIN
+	tb_ut : ENTITY work.tb_common_paged_ram_crw_crw
+		PORT MAP(
+			o_rst => rst,
+			o_clk => clk,
+			o_tb_end => tb_end,
+			o_test_msg => test_msg,
+			o_test_pass => test_pass
+		);
+
+	p_vunit : PROCESS
+	BEGIN
+		test_runner_setup(runner, runner_cfg);
+		wait until tb_end = '1';
+		test_runner_cleanup(runner);
+		wait;
+	END PROCESS;
+
+	p_verify : PROCESS(rst, clk)
+	BEGIN
+		IF rst = '0' THEN
+			IF rising_edge(clk) THEN
+				check(test_pass, "Test Failed: " & test_msg);
+			END IF;
+		END IF;
+
+	END PROCESS;
+END tb;
