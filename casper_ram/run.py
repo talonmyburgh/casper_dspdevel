e481f30f96626dce946ca1edf26754d090fc671b
RocketRoss <code@radonn.co.za>
2021-11-08 17:38:31 +0200

    *+ casper_ram run.py fixed and test

--- casper_ram/run.py
+++ casper_ram/run.py
@@ -1,5 +1,5 @@
 from vunit import VUnit
-import os
+import os, glob
 
 # Create VUnit instance by parsing command line arguments
 script_dir = os.path.dirname(__file__)
@@ -10,14 +10,15 @@ lib = vu.add_library("xpm")
 lib.add_source_files(os.path.join(script_dir, "../xilinx/xpm_vhdl/src/xpm/xpm_VCOMP.vhd"))
 lib.add_source_files(os.path.join(script_dir, "../xilinx/xpm_vhdl/src/xpm/xpm_memory/hdl/*.vhd"))
 
-# lib0 = vu.add_library("altera_mf")
-# lib0.add_source_files(os.path.join(script_dir, "../intel/altera_mf/*.vhd"))
+lib0 = vu.add_library("altera_mf")
+lib0.add_source_files(os.path.join(script_dir, "../intel/altera_mf/altera_mf_components.vhd"))
+altera_mf_source_file = lib0.add_source_files(os.path.join(script_dir, "../intel/altera_mf/altera_mf.vhd"))
 
 lib1 = vu.add_library("casper_ram_lib")
 lib1.add_source_files(os.path.join(script_dir, "./*.vhd"))
 
 TB_GENERATED = lib1.test_bench("tb_tb_vu_common_paged_ram_crw_crw")
-TB_GENERATED.add_config(name = "common_paged_ram_crw_crw_test")
+TB_GENERATED.add_config(name="common_paged_ram_crw_crw_test")
 
 lib2 = vu.add_library("common_components_lib")
 lib2.add_source_files(os.path.join(script_dir, "../common_components/*.vhd"))
@@ -29,7 +30,9 @@ lib4 = vu.add_library("ip_xpm_ram_lib")
 lib4.add_source_files(os.path.join(script_dir, "../ip_xpm/ram/*.vhd"))
 
 lib5 = vu.add_library("ip_stratixiv_ram_lib")
-lib5.add_source_files(os.path.join(script_dir, "../ip_stratixiv/ram/*.vhd"))
+ip_stratix_source_files = lib5.add_source_files(os.path.join(script_dir, "../ip_stratixiv/ram/*.vhd"))
+for ip_stratix_source_file in ip_stratix_source_files:
+	ip_stratix_source_file.add_dependency_on(altera_mf_source_file)
 
 lib6 = vu.add_library("technology_lib",allow_duplicate = True)
 lib6.add_source_files(os.path.join(script_dir, "../technology/technology_select_pkg.vhd"))
