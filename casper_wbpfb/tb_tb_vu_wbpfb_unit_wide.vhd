47351c6b74fb2eb1d64127b1eeb9278463d5aa80
RocketRoss <code@radonn.co.za>
2021-11-25 12:12:28 +0200

    ^ casper_wbpfb tb_tb_vu and run.py. Tests fail

--- casper_wbpfb/tb_tb_vu_wbpfb_unit_wide.vhd
+++ casper_wbpfb/tb_tb_vu_wbpfb_unit_wide.vhd
@@ -1,13 +1,14 @@
-library ieee, std, common_pkg_lib, r2sdf_lib, casper_filter_lib, vunit_lib;
+library ieee, std, common_pkg_lib, r2sdf_fft_lib, casper_filter_lib, wb_fft_lib, wpfb_lib, vunit_lib;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.all;
 use IEEE.std_logic_textio.all;
 use STD.textio.all;
-use r2sdf_lib.rTwoSDFPkg.all;
+use r2sdf_fft_lib.rTwoSDFPkg.all;
 use casper_filter_lib.fil_pkg.all;
 use common_pkg_lib.common_pkg.all;
-use work.fft_gnrcs_intrfcs_pkg.all;
+use wb_fft_lib.fft_gnrcs_intrfcs_pkg.all;
+use wpfb_lib.wbpfb_gnrcs_intrfcs_pkg.all;
 context vunit_lib.vunit_context;
 
 entity tb_tb_vu_wbpfb_unit_wide is
@@ -31,6 +32,8 @@ entity tb_tb_vu_wbpfb_unit_wide is
         g_guard_w               : natural;  -- = 2,  Guard used to avoid overflow in FFT stage. 
         g_guard_enable          : boolean;  -- = true when input needs guarding, false when input requires no guarding but scaling must be skipped at the last stage(s) (used in wb fft)
         g_diff_margin           : integer    := 2;  -- maximum difference between HDL output and expected output (> 0 to allow minor rounding differences)
+        g_coefs_file_prefix_ab  : string     := "data/run_pfb_m_pfir_coeff_fircls1";
+        g_coefs_file_prefix_c   : string     := "data/run_pfb_complex_m_pfir_coeff_fircls1";
         g_data_file_a           : string     := "data/run_pfft_m_sinusoid_chirp_8b_32points_16b.dat";
         g_data_file_a_nof_lines : natural    := 6400;
         g_data_file_b           : string     := "UNUSED";
@@ -55,7 +58,32 @@ architecture tb of tb_tb_vu_wbpfb_unit_wide is
 	SIGNAL test_pass	: BOOLEAN;
 
     CONSTANT c_nof_blk_per_sync          : natural := 20;
-    CONSTANT c_wpfb_vu : t_wpfb := (g_wb_factor, g_nof_points, g_nof_chan, g_nof_wb_streams, g_nof_taps, g_fil_backoff_w, g_fil_in_dat_w, g_fil_out_dat_w, g_coef_dat_w, g_use_reorder, g_use_fft_shift, g_use_separate, g_fft_in_dat_w, g_fft_out_dat_w, g_fft_out_gain_w, g_stage_dat_w, g_guard_w, g_guard_enable, 56, 2,c_nof_blk_per_sync,c_fft_pipeline,c_fft_pipeline,c_fil_ppf_pipeline);
+    CONSTANT c_wpfb_vu : t_wpfb := (
+        wb_factor => g_wb_factor,
+        nof_points => g_nof_points,
+        nof_chan => g_nof_chan,
+        nof_wb_streams => g_nof_wb_streams,
+        nof_taps => g_nof_taps,
+        fil_backoff_w => g_fil_backoff_w,
+        fil_in_dat_w => g_fil_in_dat_w,
+        fil_out_dat_w => g_fil_out_dat_w,
+        coef_dat_w => g_coef_dat_w,
+        use_reorder => g_use_reorder,
+        use_fft_shift => g_use_fft_shift,
+        use_separate => g_use_separate,
+        fft_in_dat_w => g_fft_in_dat_w,
+        fft_out_dat_w => g_fft_out_dat_w,
+        fft_out_gain_w => g_fft_out_gain_w,
+        stage_dat_w => g_stage_dat_w,
+        guard_w => g_guard_w,
+        guard_enable => g_guard_enable,
+        stat_data_w => 56,
+        stat_data_sz => 2,
+        nof_blk_per_sync => c_nof_blk_per_sync,
+        pft_pipeline => c_fft_pipeline,
+        fft_pipeline => c_fft_pipeline,
+        fil_pipeline => c_fil_ppf_pipeline
+        );
 BEGIN
 	tb_ut : ENTITY work.tb_wbpfb_unit_wide
         GENERIC MAP(
