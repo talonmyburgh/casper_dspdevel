e0a748a7f20c3be09c59e2b1cd0be276cf7c2dff
talonmyburgh <myburgh.talon@gmail.com>
2021-11-25 10:50:38 +0200

    +* Regression testing for wbpfb. Not finished

--- casper_wbpfb/tb_tb_vu_wbpfb_unit_wide.vhd
+++ casper_wbpfb/tb_tb_vu_wbpfb_unit_wide.vhd
@@ -1,9 +1,11 @@
-library ieee, std, common_pkg_lib, vunit_lib;
+library ieee, std, common_pkg_lib, r2sdf_lib, casper_filter_lib, vunit_lib;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.all;
 use IEEE.std_logic_textio.all;
 use STD.textio.all;
+use r2sdf_lib.rTwoSDFPkg.all;
+use casper_filter_lib.fil_pkg.all;
 use common_pkg_lib.common_pkg.all;
 use work.fft_gnrcs_intrfcs_pkg.all;
 context vunit_lib.vunit_context;
@@ -14,7 +16,11 @@ entity tb_tb_vu_wbpfb_unit_wide is
         g_nof_points            : natural;  -- = 1024, N point FFT
         g_nof_chan              : natural;  -- = default 0, defines the number of channels (=time-multiplexed input signals): nof channels = 2**nof_chan         
         g_nof_wb_streams        : natural;
-        
+        g_nof_taps              : natural;
+        g_fil_backoff_w         : natural;
+        g_fil_in_dat_w          : natural;
+        g_fil_out_dat_w         : natural;
+        g_coef_dat_w            : natural;
         g_use_reorder           : boolean;  -- = false for bit-reversed output, true for normal output
         g_use_fft_shift         : boolean;  -- = false for [0, pos, neg] bin frequencies order, true for [neg, 0, pos] bin frequencies order in case of complex input
         g_use_separate          : boolean;  -- = false for complex input, true for two real inputs
@@ -48,13 +54,15 @@ architecture tb of tb_tb_vu_wbpfb_unit_wide is
 	SIGNAL test_msg     : STRING(1 to 80);
 	SIGNAL test_pass	: BOOLEAN;
 
-    CONSTANT c_fft_vu : t_fft := (g_use_reorder, g_use_fft_shift, g_use_separate, g_nof_chan, g_wb_factor, g_twiddle_offset, g_nof_points, g_in_dat_w, g_out_dat_w, g_out_gain_w, g_stage_dat_w, g_guard_w, g_guard_enable, 56, 2);
-
+    CONSTANT c_nof_blk_per_sync          : natural := 20;
+    CONSTANT c_wpfb_vu : t_wpfb := (g_wb_factor, g_nof_points, g_nof_chan, g_nof_wb_streams, g_nof_taps, g_fil_backoff_w, g_fil_in_dat_w, g_fil_out_dat_w, g_coef_dat_w, g_use_reorder, g_use_fft_shift, g_use_separate, g_fft_in_dat_w, g_fft_out_dat_w, g_fft_out_gain_w, g_stage_dat_w, g_guard_w, g_guard_enable, 56, 2,c_nof_blk_per_sync,c_fft_pipeline,c_fft_pipeline,c_fil_ppf_pipeline);
 BEGIN
 	tb_ut : ENTITY work.tb_wbpfb_unit_wide
         GENERIC MAP(
-            g_fft => c_fft_vu,        
+            g_wpfb => c_wpfb_vu,        
             g_diff_margin => g_diff_margin,
+            g_coefs_file_prefix_ab => g_coefs_file_prefix_ab,
+            g_coefs_file_prefix_c => g_coefs_file_prefix_c,
             g_data_file_a => g_data_file_a,
             g_data_file_a_nof_lines => g_data_file_a_nof_lines,
             g_data_file_b => g_data_file_b,
@@ -62,10 +70,7 @@ BEGIN
             g_data_file_c => g_data_file_c,
             g_data_file_c_nof_lines => g_data_file_c_nof_lines,
             g_data_file_nof_lines => g_data_file_nof_lines,
-            g_enable_in_val_gaps => g_enable_in_val_gaps,
-            g_use_variant => g_use_variant,
-            g_ovflw_behav => g_ovflw_behav,
-            g_use_round => g_use_round
+            g_enable_in_val_gaps => g_enable_in_val_gaps
         )
 		PORT MAP(
 			o_rst => rst,
