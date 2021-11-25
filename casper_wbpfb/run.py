09f611ca33b9e1b40fd9cc80fcf4b62055ed96f8
RocketRoss <code@radonn.co.za>
2021-11-25 12:12:28 +0200

    ^ casper_wbpfb tb_tb_vu and run.py. Tests fail

--- casper_wbpfb/run.py
+++ casper_wbpfb/run.py
@@ -34,10 +34,10 @@ ip_mult_infer = ip_xpm_mult_lib.add_source_file(join(script_dir, "../ip_xpm/mult
 ip_stratixiv_mult_lib = vu.add_library("ip_stratixiv_mult_lib", allow_duplicate=True)
 ip_stratixiv_complex_mult_rtl = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_complex_mult_rtl.vhd"))
 ip_stratixiv_complex_mult = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_complex_mult.vhd"))
-ip_stratixiv_mult = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_mult_rtl.vhd"))
-ip_stratixiv_mult_rtl = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_mult.vhd"))
+ip_stratixiv_mult_rtl = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_mult_rtl.vhd"))
+# ip_stratixiv_mult = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_mult.vhd"))
 ip_stratixiv_complex_mult.add_dependency_on(altera_mf_source_file)
-ip_stratixiv_mult.add_dependency_on(altera_mf_source_file)
+# ip_stratixiv_mult.add_dependency_on(altera_mf_source_file)
 
 # XPM RAM library
 ip_xpm_ram_lib = vu.add_library("ip_xpm_ram_lib")
@@ -53,7 +53,7 @@ ip_stratix_file_crw_crw = ip_stratixiv_ram_lib.add_source_file(join(script_dir, 
 ip_stratix_file_cr_cw.add_dependency_on(altera_mf_source_file)
 ip_stratix_file_crw_crw.add_dependency_on(altera_mf_source_file)
 
-# COMMON COMPONENTS Library 
+# COMMON COMPONENTS Library
 common_components_lib = vu.add_library("common_components_lib")
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_pipeline.vhd"))
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_bit_delay.vhd"))
@@ -61,6 +61,8 @@ common_components_lib.add_source_files(join(script_dir, "../common_components/co
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_delay.vhd"))
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_paged_reg.vhd"))
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_switch.vhd"))
+common_components_lib.add_source_files(join(script_dir, "../common_components/common_select_symbol.vhd"))
+common_components_lib.add_source_files(join(script_dir, "../common_components/common_components_pkg.vhd"))
 
 # COMMON PACKAGE Library
 common_pkg_lib = vu.add_library("common_pkg_lib")
@@ -71,13 +73,13 @@ common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/common_lfsr_sequ
 
 # DP PACKAGE Library
 dp_pkg_lib = vu.add_library("dp_pkg_lib")
-dp_pkg_lib.add_source_file(join(script_dir,"../casper_dp_pkg/dp_stream_pkg.vhd"))
+dp_pkg_lib.add_source_file(join(script_dir, "../casper_dp_pkg/dp_stream_pkg.vhd"))
 
 # DP COMPONENTS Library
 dp_components_lib = vu.add_library("dp_components_lib")
-dp_components_lib.add_source_file(join(script_dir,"../casper_dp_components/dp_hold_input.vhd"))
-dp_components_lib.add_source_file(join(script_dir,"../casper_dp_components/dp_hold_ctrl.vhd"))
-dp_components_lib.add_source_file(join(script_dir,"../casper_dp_components/dp_block_gen.vhd"))
+dp_components_lib.add_source_file(join(script_dir, "../casper_dp_components/dp_hold_input.vhd"))
+dp_components_lib.add_source_file(join(script_dir, "../casper_dp_components/dp_hold_ctrl.vhd"))
+dp_components_lib.add_source_file(join(script_dir, "../casper_dp_components/dp_block_gen.vhd"))
 
 # TECHNOLOGY Library
 technology_lib = vu.add_library("technology_lib")
@@ -89,9 +91,9 @@ casper_counter_lib.add_source_file(join(script_dir, "../casper_counter/common_co
 
 # CASPER ADDER Library
 casper_adder_lib = vu.add_library("casper_adder_lib")
-casper_adder_lib.add_source_file(join(script_dir,"../casper_adder/common_add_sub.vhd"))
-casper_adder_lib.add_source_file(join(script_dir,"../casper_adder/common_adder_tree.vhd"))
-casper_adder_lib.add_source_file(join(script_dir,"../casper_adder/common_adder_tree_a_str.vhd"))
+casper_adder_lib.add_source_file(join(script_dir, "../casper_adder/common_add_sub.vhd"))
+casper_adder_lib.add_source_file(join(script_dir, "../casper_adder/common_adder_tree.vhd"))
+casper_adder_lib.add_source_file(join(script_dir, "../casper_adder/common_adder_tree_a_str.vhd"))
 
 # CASPER MUlTIPLIER Library
 casper_multiplier_lib = vu.add_library("casper_multiplier_lib")
@@ -134,83 +136,93 @@ casper_ram_lib.add_source_file(join(script_dir, "../casper_ram/common_paged_ram_
 
 # CASPER FILTER Library
 casper_filter_lib = vu.add_library("casper_filter_lib")
-casper_filter_lib.add_source_file(join(script_dir,"../casper_filter/fil_pkg.vhd"))
-casper_filter_lib.add_source_file(join(script_dir,"../casper_filter/fil_ppf_ctrl.vhd"))
-casper_filter_lib.add_source_file(join(script_dir,"../casper_filter/fil_ppf_filter.vhd"))
-casper_filter_lib.add_source_file(join(script_dir,"../casper_filter/fil_ppf_single.vhd"))
-casper_filter_lib.add_source_file(join(script_dir,"../casper_filter/fil_ppf_wide.vhd"))
+casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_pkg.vhd"))
+casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_ctrl.vhd"))
+casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_filter.vhd"))
+casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_single.vhd"))
+casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_wide.vhd"))
 
 # CASPER MEM Library
 casper_mm_lib = vu.add_library("casper_mm_lib")
-casper_mm_lib.add_source_file(join(script_dir,"../casper_mm/tb_common_mem_pkg.vhd"))
+casper_mm_lib.add_source_file(join(script_dir, "../casper_mm/tb_common_mem_pkg.vhd"))
 
 # CASPER SIM TOOLS Library
-casper_sim_tools_lib =vu.add_library("casper_sim_tools_lib")
-casper_sim_tools_lib.add_source_file(join(script_dir,"../casper_sim_tools/common_wideband_data_scope.vhd"))
+casper_sim_tools_lib = vu.add_library("casper_sim_tools_lib")
+casper_sim_tools_lib.add_source_file(join(script_dir, "../casper_sim_tools/common_wideband_data_scope.vhd"))
+
+# CASPER PIPELINE Library
+casper_pipeline_lib = vu.add_library("casper_pipeline_lib")
+casper_pipeline_lib.add_source_file(join(script_dir, "../casper_pipeline/dp_pipeline.vhd"))
+
+# CASPER DIAGNOSTICS Library
+casper_diagnostics_lib = vu.add_library("casper_diagnostics_lib")
+casper_diagnostics_lib.add_source_file(join(script_dir, "../casper_diagnostics/diag_pkg.vhd"))
 
 # RTWOSDF Library
 r2sdf_fft_lib = vu.add_library("r2sdf_fft_lib")
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoBF.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoBFStage.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoOrder.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/twiddlesPkg.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoSDFPkg.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoWeights.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoWMul.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoSDFStage.vhd"))
-r2sdf_fft_lib.add_source_file(join(script_dir,"../r2sdf_fft/rTwoSDF.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoBF.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoBFStage.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoOrder.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/twiddlesPkg.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoSDFPkg.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoWeights.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoWMul.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoSDFStage.vhd"))
+r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoSDF.vhd"))
 
 # WIDEBAND FFT Library
 wb_fft_lib = vu.add_library("wb_fft_lib")
-wb_fft_lib.add_source_file(join(script_dir,"../casper_wb_fft/fft_sepa.vhd"))
-wb_fft_lib.add_source_file(join(script_dir,"../casper_wb_fft/fft_gnrcs_intrfcs_pkg.vhd"))
-wb_fft_lib.add_source_file(join(script_dir,"../casper_wb_fft/tb_fft_pkg.vhd"))
-wb_fft_lib.add_source_file(join(script_dir,"../casper_wb_fft/fft_reorder_sepa_pipe.vhd"))
-wb_fft_lib.add_source_file(join(script_dir,"../casper_wb_fft/fft_r2_pipe.vhd"))
-wb_fft_lib.add_source_file(join(script_dir,"../casper_wb_fft/fft_r2_bf_par.vhd"))
-wb_fft_lib.add_source_file(join(script_dir,"../casper_wb_fft/fft_r2_par.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_sepa.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_gnrcs_intrfcs_pkg.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/tb_fft_pkg.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_reorder_sepa_pipe.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_r2_pipe.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_r2_bf_par.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_r2_par.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_r2_wide.vhd"))
+wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_sepa_wide.vhd"))
 
 # WIDEBAND PFB Library
-wbpfb_lib = vu.add_library("wbpfb_lib")
-wbpfb_lib.add_source_file(join(script_dir,"wbpfb_gnrcs_intrfcs_pkg.vhd"))
-wbpfb_lib.add_source_file(join(script_dir,"dp_bsn_restore_global.vhd"))
-wbpfb_lib.add_source_file(join(script_dir,"dp_block_gen_valid_arr.vhd"))
-wbpfb_lib.add_source_file(join(script_dir,"wbpfb_unit_dev.vhd"))
-wbpfb_lib.add_source_file(join(script_dir,"tb_wbpfb_unit_wide.vhd"))
-wbpfb_lib.add_source_file(join(script_dir,"tb_tb_vu_wbpfb_unit_wide.vhd"))
+wpfb_lib = vu.add_library("wpfb_lib")
+wpfb_lib.add_source_file(join(script_dir, "wbpfb_gnrcs_intrfcs_pkg.vhd"))
+wpfb_lib.add_source_file(join(script_dir, "dp_bsn_restore_global.vhd"))
+wpfb_lib.add_source_file(join(script_dir, "dp_block_gen_valid_arr.vhd"))
+wpfb_lib.add_source_file(join(script_dir, "wbpfb_unit_dev.vhd"))
+wpfb_lib.add_source_file(join(script_dir, "tb_wbpfb_unit_wide.vhd"))
+wpfb_lib.add_source_file(join(script_dir, "tb_tb_vu_wbpfb_unit_wide.vhd"))
 
 # CONSTANTS FOR TESTS
 c_stage_dat_extra_w = 28
-c_pre_ab              = "../../../../../data/mem/hex/run_pfb_m_pfir_coeff_fircls1_16taps_32points_16b"                 
-c_pre_ab_1024         = "../../../../../data/mem/hex/run_pfb_m_pfir_coeff_fircls1_16taps_1024points_16b"                 
-c_pre_ab_v2           = "../../../../../data/mem/hex/run_pfb_m_v2_pfir_coeff_fircls1_16taps_1024points_16b"              
+c_pre_ab              = "../../../../../data/mem/hex/run_pfb_m_pfir_coeff_fircls1_16taps_32points_16b"
+c_pre_ab_1024         = "../../../../../data/mem/hex/run_pfb_m_pfir_coeff_fircls1_16taps_1024points_16b"
+c_pre_ab_v2           = "../../../../../data/mem/hex/run_pfb_m_v2_pfir_coeff_fircls1_16taps_1024points_16b"
 c_pre_c               = "../../../../../data/mem/hex/run_pfb_complex_m_pfir_coeff_fircls1_16taps_32points_16b"
 c_pre_c_64            = "../../../../../data/mem/hex/run_pfb_complex_m_pfir_coeff_fircls1_16taps_64points_16b"
 c_pre_c_1024          = "../../../../../data/mem/hex/run_pfb_complex_m_pfir_coeff_fircls1_16taps_1024points_16b"
-  
-c_sinusoid_chirp_1024 = "../../../../../data/run_pfb_m_sinusoid_chirp_8b_16taps_1024points_16b.dat"   
-c_sinusoid_chirp      = "../../../../../data/run_pfb_m_sinusoid_chirp_8b_16taps_32points_16b.dat"     
-c_sinusoid_1024       = "../../../../../data/run_pfb_m_sinusoid_8b_16taps_1024points_16b.dat"         
-c_sinusoid_1024_v2    = "../../../../../data/run_pfb_m_v2_sinusoid_8b_16taps_1024points_16b.dat"      
-c_sinusoid            = "../../../../../data/run_pfb_m_sinusoid_8b_16taps_32points_16b.dat"           
-c_impulse_chirp       = "../../../../../data/run_pfb_m_impulse_chirp_8b_16taps_32points_16b.dat"      
-c_noise_1024          = "../../../../../data/run_pfb_m_noise_8b_16taps_1024points_16b.dat"            
-c_noise               = "../../../../../data/run_pfb_m_noise_8b_16taps_32points_16b.dat"              
-c_dc_agwn             = "../../../../../data/run_pfb_m_dc_agwn_8b_16taps_32points_16b.dat"            
 
-c_phasor_chirp_1024   = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_1024points_16b.dat"   
-c_phasor_chirp_128    = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_128points_16b.dat"    
-c_phasor_chirp_64     = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_64points_16b.dat"     
-c_phasor_chirp        = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_32points_16b.dat"     
-c_phasor              = "../../../../../data/run_pfb_complex_m_phasor_8b_16taps_32points_16b.dat"           
-c_noise_complex_1024  = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_1024points_16b.dat"  
-c_noise_complex_128   = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_128points_16b.dat"   
-c_noise_complex_64    = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_64points_16b.dat"    
-c_noise_complex       = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_32points_16b.dat"    
+c_sinusoid_chirp_1024 = "../../../../../data/run_pfb_m_sinusoid_chirp_8b_16taps_1024points_16b.dat"
+c_sinusoid_chirp      = "../../../../../data/run_pfb_m_sinusoid_chirp_8b_16taps_32points_16b.dat"
+c_sinusoid_1024       = "../../../../../data/run_pfb_m_sinusoid_8b_16taps_1024points_16b.dat"
+c_sinusoid_1024_v2    = "../../../../../data/run_pfb_m_v2_sinusoid_8b_16taps_1024points_16b.dat"
+c_sinusoid            = "../../../../../data/run_pfb_m_sinusoid_8b_16taps_32points_16b.dat"
+c_impulse_chirp       = "../../../../../data/run_pfb_m_impulse_chirp_8b_16taps_32points_16b.dat"
+c_noise_1024          = "../../../../../data/run_pfb_m_noise_8b_16taps_1024points_16b.dat"
+c_noise               = "../../../../../data/run_pfb_m_noise_8b_16taps_32points_16b.dat"
+c_dc_agwn             = "../../../../../data/run_pfb_m_dc_agwn_8b_16taps_32points_16b.dat"
+
+c_phasor_chirp_1024   = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_1024points_16b.dat"
+c_phasor_chirp_128    = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_128points_16b.dat"
+c_phasor_chirp_64     = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_64points_16b.dat"
+c_phasor_chirp        = "../../../../../data/run_pfb_complex_m_phasor_chirp_8b_16taps_32points_16b.dat"
+c_phasor              = "../../../../../data/run_pfb_complex_m_phasor_8b_16taps_32points_16b.dat"
+c_noise_complex_1024  = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_1024points_16b.dat"
+c_noise_complex_128   = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_128points_16b.dat"
+c_noise_complex_64    = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_64points_16b.dat"
+c_noise_complex       = "../../../../../data/run_pfb_complex_m_noise_complex_8b_16taps_32points_16b.dat"
 
 c_zero                = "UNUSED"
 c_un                  = "UNUSED"
- 
+
 ##CONSTANT t_wpfb#########################################################################################################
 
 # wb 1, two real
@@ -304,9 +316,9 @@ c_wb4_complex_flipped_channels.update({'g_nof_chan':2})
 ##########################################################################################################################
 
 ##Test bench dictionaries and configurations#########################################################################################################
-TB_GENERATED = wbpfb_lib.test_bench('tb_tb_vu_wbpfb_unit_wide')
+TB_GENERATED = wpfb_lib.test_bench('tb_tb_vu_wbpfb_unit_wide')
 # u_act_wb4_two_real_a0_1024
-c_act_wb4_two_real_a0_1024 = dict( 
+c_act_wb4_two_real_a0_1024 = dict(
     g_diff_margin = 1,
     g_coefs_file_prefix_ab = c_pre_ab_v2,
     g_coefs_file_prefix_c = c_pre_c_1024,
@@ -351,7 +363,7 @@ TB_GENERATED.add_config(
 )
 
 # u_act_wb1_two_real_chirp
-c_act_wb1_two_real_chirp = dict( 
+c_act_wb1_two_real_chirp = dict(
     g_diff_margin = 5,
     g_coefs_file_prefix_ab = c_pre_ab,
     g_coefs_file_prefix_c = c_pre_c,
@@ -387,7 +399,7 @@ TB_GENERATED.add_config(
 )
 
 # u_rnd_wb4_two_real_noise
-c_rnd_wb4_two_real_noise=dict( 
+c_rnd_wb4_two_real_noise=dict(
     g_diff_margin = 5,
     g_coefs_file_prefix_ab = c_pre_ab,
     g_coefs_file_prefix_c = c_pre_c,
@@ -447,7 +459,7 @@ TB_GENERATED.add_config(
 )
 
 # u_act_wb1_complex_chirp_1024
-c_act_wb1_complex_chirp_1024=dict( 
+c_act_wb1_complex_chirp_1024=dict(
     g_diff_margin = 3,
     g_coefs_file_prefix_ab = c_pre_ab_1024,
     g_coefs_file_prefix_c = c_pre_c_1024,
@@ -475,7 +487,7 @@ TB_GENERATED.add_config(
 )
 
 # u_act_wb1_complex_chirp_64
-c_act_wb1_complex_chirp_64 = dict( 
+c_act_wb1_complex_chirp_64 = dict(
     g_diff_margin = 3,
     g_coefs_file_prefix_ab = c_pre_ab,
     g_coefs_file_prefix_c = c_pre_c_64,
@@ -520,7 +532,7 @@ TB_GENERATED.add_config(
 )
 
 # u_act_wb4_complex_chirp
-c_act_wb4_complex_chirp = dict( 
+c_act_wb4_complex_chirp = dict(
     g_diff_margin = 3,
     g_coefs_file_prefix_ab = c_pre_ab,
     g_coefs_file_prefix_c = c_pre_c,
@@ -556,7 +568,7 @@ TB_GENERATED.add_config(
 )
 
 # u_rnd_wb1_complex_phasor
-c_rnd_wb1_complex_phasor= dict( 
+c_rnd_wb1_complex_phasor= dict(
     g_diff_margin = 3,
     g_coefs_file_prefix_ab = c_pre_ab,
     g_coefs_file_prefix_c = c_pre_c,
@@ -648,7 +660,7 @@ TB_GENERATED.add_config(
 )
 
 # Run vunit function
-vu.set_compile_option("ghdl.a_flags", ["-frelaxed","-fsynopsys","-fexplicit","-Wno-hide"])
-vu.set_sim_option("ghdl.elab_flags", ["-frelaxed","-fsynopsys","-fexplicit","--syn-binding"])
+vu.set_compile_option("ghdl.a_flags", ["-frelaxed", "-fsynopsys", "-fexplicit", "-Wno-hide"])
+vu.set_sim_option("ghdl.elab_flags", ["-frelaxed", "-fsynopsys", "-fexplicit", "--syn-binding"])
 vu.set_sim_option("ghdl.sim_flags", ["--ieee-asserts=disable"])
 vu.main()
