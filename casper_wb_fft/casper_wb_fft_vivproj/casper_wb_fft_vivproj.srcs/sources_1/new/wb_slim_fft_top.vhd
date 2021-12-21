fd2c7c72b9824f7a94a70760dc9373815497b787
talonmyburgh <myburgh.talon@gmail.com>
2021-12-21 11:24:32 +0200

    ** minor changes

--- casper_wb_fft/casper_wb_fft_vivproj/casper_wb_fft_vivproj.srcs/sources_1/new/wb_slim_fft_top.vhd
+++ casper_wb_fft/casper_wb_fft_vivproj/casper_wb_fft_vivproj.srcs/sources_1/new/wb_slim_fft_top.vhd
@@ -7,21 +7,21 @@ use r2sdf_fft_lib.rTwoSDFPkg.all;
 --Purpose: A Simulink necessary wrapper for the fft_wide_unit. Serves to expose all signals and generics individually.
 entity wideband_fft_top is
 	generic(
-		use_reorder    : boolean := c_use_reorder;       -- = false for bit-reversed output, true for normal output
-		use_fft_shift  : boolean := c_use_fft_shift;       -- = false for [0, pos, neg] bin frequencies order, true for [neg, 0, pos] bin frequencies order in case of complex input
-		use_separate   : boolean := c_use_separate;       -- = false for complex input, true for two real inputs
-		nof_chan       : natural := c_nof_chan;       -- = default 0, defines the number of channels (=time-multiplexed input signals): nof channels = 2**nof_chan 
+		use_reorder    : boolean := c_fft_use_reorder;       -- = false for bit-reversed output, true for normal output
+		use_fft_shift  : boolean := c_fft_use_fft_shift;       -- = false for [0, pos, neg] bin frequencies order, true for [neg, 0, pos] bin frequencies order in case of complex input
+		use_separate   : boolean := c_fft_use_separate;       -- = false for complex input, true for two real inputs
+		nof_chan       : natural := c_fft_nof_chan;       -- = default 0, defines the number of channels (=time-multiplexed input signals): nof channels = 2**nof_chan 
 		wb_factor      : natural := c_fft_wb_factor;       -- = default 1, wideband factor
-		twiddle_offset : natural := c_twiddle_offset;       -- = default 0, twiddle offset for PFT sections in a wideband FFT
-		nof_points     : natural := c_nof_points;       -- = 1024, N point FFT
+		twiddle_offset : natural := c_fft_twiddle_offset;       -- = default 0, twiddle offset for PFT sections in a wideband FFT
+		nof_points     : natural := c_fft_nof_points;       -- = 1024, N point FFT
 		in_dat_w       : natural := c_fft_in_dat_w;       -- = 8,  number of input bits
 		out_dat_w      : natural := c_fft_out_dat_w;       -- = 13, number of output bits
-		out_gain_w     : natural := c_out_gain_w;       -- = 0, output gain factor applied after the last stage output, before requantization to out_dat_w
+		out_gain_w     : natural := c_fft_out_gain_w;       -- = 0, output gain factor applied after the last stage output, before requantization to out_dat_w
 		stage_dat_w    : natural := c_fft_stage_dat_w;       -- = 18, data width used between the stages(= DSP multiplier-width)
-		guard_w        : natural := c_guard_w;       -- = 2, guard used to avoid overflow in first FFT stage, compensated in last guard_w nof FFT stages. 
+		guard_w        : natural := c_fft_guard_w;       -- = 2, guard used to avoid overflow in first FFT stage, compensated in last guard_w nof FFT stages. 
                                                     --   on average the gain per stage is 2 so guard_w = 1, but the gain can be 1+sqrt(2) [Lyons section
                                                     --   12.3.2], therefore use input guard_w = 2.
-		guard_enable   : boolean := c_guard_enable       -- = true when input needs guarding, false when input requires no guarding but scaling must be
+		guard_enable   : boolean := c_fft_guard_enable       -- = true when input needs guarding, false when input requires no guarding but scaling must be
                                                     --   skipped at the last stage(s) compensate for input guard (used in wb fft with pipe fft section
                                                     --   doing the input guard and par fft section doing the output compensation)
     );
@@ -57,8 +57,8 @@ end entity wideband_fft_top;
 architecture RTL of wideband_fft_top is
         constant cc_fft : t_fft := (use_reorder,use_fft_shift,use_separate,nof_chan,wb_factor,
         twiddle_offset,nof_points, in_dat_w,out_dat_w,out_gain_w,stage_dat_w,guard_w,guard_enable, 56, 2);
-        signal in_bb_sosi_arr : t_bb_sosi_arr_in(wb_factor - 1 downto 0);
-        signal out_bb_sosi_arr : t_bb_sosi_arr_out(wb_factor - 1 downto 0);
+        signal in_bb_sosi_arr : t_fft_sosi_arr_in(wb_factor - 1 downto 0);
+        signal out_bb_sosi_arr : t_fft_sosi_arr_out(wb_factor - 1 downto 0);
         constant c_pft_pipeline : t_fft_pipeline := c_fft_pipeline;
         constant c_fft_pipeline : t_fft_pipeline := c_fft_pipeline;
         begin
@@ -72,9 +72,9 @@ architecture RTL of wideband_fft_top is
         rst       => rst,
         clk       => clk,
         shiftreg => in_shiftreg,
-        in_bb_sosi_arr  => in_bb_sosi_arr,
+        in_fft_sosi_arr  => in_bb_sosi_arr,
         ovflw => out_ovflw,
-        out_bb_sosi_arr => out_bb_sosi_arr);
+        out_fft_sosi_arr => out_bb_sosi_arr);
         
         otherinprtmap: for j in 0 to wb_factor-1 generate
         in_bb_sosi_arr(j).sync <= in_sync;
