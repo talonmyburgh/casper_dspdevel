412032c038dfb22d3ff7d3ecb4f43cced1d0ae98
talonmyburgh <myburgh.talon@gmail.com>
2022-05-31 20:33:54 +0200

    ^* working edge_detect module

--- misc/edge_detect.vhd
+++ misc/edge_detect.vhd
@@ -5,7 +5,7 @@ entity edge_detect is
     generic (
         g_edge_type   : STRING := "rising";
         g_output_pol  : STRING := "high"
-    )
+    );
     port (
         clk         : IN std_logic;
         ce          : IN std_logic;
@@ -15,12 +15,12 @@ entity edge_detect is
 END edge_detect;
 
 architecture rtl of edge_detect is
-   SIGNAL s_not_in_sig : std_logic_vector(in_sig'RANGE);
-   SIGNAL s_delayed_in_sig : std_logic_vector(in_sig'RANGE);
+   SIGNAL s_not_in_sig : std_logic_vector(in_sig'RANGE) := (others=>'0');
+   SIGNAL s_delayed_in_sig : std_logic_vector(in_sig'RANGE) := (others=>'0');
    SIGNAL s_out_sig  : std_logic_vector(in_sig'RANGE);
 begin
 
-    s_not_in_sig <= not in_sign;
+    s_not_in_sig <= not in_sig;
 
 --------------------------------------------------------------
 -- one clock cycle delay
@@ -38,19 +38,19 @@ END PROCESS;
 nor_gate_gen: if g_edge_type = "rising" and g_output_pol = "high" generate
      s_out_sig <= s_not_in_sig nor s_delayed_in_sig;
 end generate;
-nor_gate_gen: if g_edge_type = "rising" and g_output_pol = "low" generate
+or_gate_gen: if g_edge_type = "rising" and g_output_pol = "low" generate
      s_out_sig <= s_not_in_sig or s_delayed_in_sig;
 end generate;
-nor_gate_gen: if g_edge_type = "falling" and g_output_pol = "low" generate
+nand_gate_gen: if g_edge_type = "falling" and g_output_pol = "low" generate
      s_out_sig <= s_not_in_sig nand s_delayed_in_sig;
 end generate;
-nor_gate_gen: if g_edge_type = "falling" and g_output_pol = "high" generate
+and_gate_gen: if g_edge_type = "falling" and g_output_pol = "high" generate
      s_out_sig <= s_not_in_sig and s_delayed_in_sig;
 end generate;
-nor_gate_gen: if g_edge_type = "both" and g_output_pol = "high" generate
+xnor_gate_gen: if g_edge_type = "both" and g_output_pol = "high" generate
      s_out_sig <= s_not_in_sig xnor s_delayed_in_sig;
 end generate;
-nor_gate_gen: if g_edge_type = "both" and g_output_pol = "low" generate
+xor_gate_gen: if g_edge_type = "both" and g_output_pol = "low" generate
      s_out_sig <= s_not_in_sig xor s_delayed_in_sig;
 end generate;
 
