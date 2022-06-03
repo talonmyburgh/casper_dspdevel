c6e773c516f496b5bf8c1f76bcf910ce79cbe0c1
talonmyburgh <myburgh.talon@gmail.com>
2022-06-03 21:41:22 +0200

    ^* module working correctly

--- misc/tb_armed_trigger.vhd
+++ misc/tb_armed_trigger.vhd
@@ -39,24 +39,35 @@ begin
         WAIT UNTIL falling_edge(clk);
         ce <= '1';
         WAIT FOR clk_period;
-        s_trig_in <= '1';
         s_arm <= '1';
-    
-        -- v_test_pass := s_out_val = s_rev_vector;
-        -- IF NOT v_test_pass THEN
-        --    v_test_msg := pad("wrong RTL result for re_out, expected: " & to_hstring(s_rev_vector) & " but got: " & to_hstring(s_out_val), o_test_msg'length, '.');
-        --    o_test_msg <= v_test_msg;
-        --    report "Error: " & v_test_msg severity error;
-        -- END IF; 
-        -- o_test_pass <= v_test_pass;
-        WAIT for clk_period *2;
+        WAIT FOR clk_period;
+        s_trig_in <= '1';
+        WAIT UNTIL rising_edge(clk);
+        v_test_pass := s_trig_out = '1';
+        IF NOT v_test_pass THEN
+           v_test_msg := pad("wrong RTL result for re_out, expected: " & std_logic'image('1') & " but got: " & std_logic'image(s_trig_out), o_test_msg'length, '.');
+           o_test_msg <= v_test_msg;
+           report "Error: " & v_test_msg severity error;
+        END IF;
+        WAIT FOR clk_period;
+        s_arm <= '0';
+        WAIT FOR clk_period;
+        s_trig_in <= '1';
+        WAIT UNTIL rising_edge(clk);
+        v_test_pass := v_test_pass or (s_trig_out = '0');
+        IF NOT v_test_pass THEN
+           v_test_msg := pad("wrong RTL result for re_out, expected: " & std_logic'image('0') & " but got: " & std_logic'image(s_trig_out), o_test_msg'length, '.');
+           o_test_msg <= v_test_msg;
+           report "Error: " & v_test_msg severity error;
+        END IF; 
+        o_test_pass <= v_test_pass;
+        WAIT for clk_period;
         tb_end <= '1';
         WAIT;
     END PROCESS;
 
     -- DUT
     u_dut : ENTITY work.armed_trigger
-
     PORT MAP(
         clk => clk,
         ce => ce,
