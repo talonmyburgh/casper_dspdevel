31a4c401a9c228f8edb9e3651eced338cb0d5827
radonnachie <code@radonn.co.za>
2022-06-11 18:28:36 +0200

    ++ flow_control/bus_expand vhdl, vunit. - this only tests one bitwidth for each expansion/slice

--- casper_flow_control/run.py
+++ casper_flow_control/run.py
@@ -16,32 +16,41 @@ common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/common_pkg.vhd")
 casper_flow_control_lib = vu.add_library("casper_flow_control_lib")
 casper_flow_control_lib.add_source_files(join(script_dir, "./*.vhd"))
 
-TB_GENERATED = casper_flow_control_lib.test_bench("tb_tb_vu_munge")
-
+TB_MUNGE = casper_flow_control_lib.test_bench("tb_tb_vu_munge")
+TB_BUS_EXPAND = casper_flow_control_lib.test_bench("tb_tb_vu_bus_expand")
 
 slice_bit_widths = [1, 4, 18]
 slice_counts = [1, 19]
 
-for bit_width, count in product(slice_bit_widths, slice_counts):
-    slice_values = random.choices(range(0, (2**bit_width)-1), k=count)
-    slice_order = random.sample(range(0, count), count)
+for count in slice_counts:
+    for bit_width in slice_bit_widths:
+        slice_values = random.choices(range(0, (2**bit_width)-1), k=count)
+        slice_order = random.sample(range(0, count), count)
 
-    slice_order_encoded = ",".join(map(str, slice_order))
-    slice_values_encoded = ",".join(map(str, slice_values))
+        slice_order_encoded = ",".join(map(str, slice_order))
+        slice_values_encoded = ",".join(map(str, slice_values))
 
-    config_name = "slice_bit_width=%s, slice_count=%s" %(bit_width, count)
-    TB_GENERATED.add_config(
+        config_name = "slice_bit_width=%s, slice_count=%s" %(bit_width, count)
+        TB_MUNGE.add_config(
+            name = config_name,
+            generics=dict(
+                g_number_of_divisions=count,
+                g_division_size_bits=bit_width,
+                g_order = slice_order_encoded,
+                g_values = slice_values_encoded
+            )
+        )
+    
+    config_name = "expansion_count=%s" %(count)
+    TB_BUS_EXPAND.add_config(
         name = config_name,
         generics=dict(
-            g_number_of_divisions=count,
-            g_division_size_bits=bit_width,
-            g_order = slice_order_encoded,
             g_values = slice_values_encoded
         )
     )
 
 config_name = "Repeated division."
-TB_GENERATED.add_config(
+TB_MUNGE.add_config(
     name = config_name,
     generics=dict(
         g_number_of_divisions=4,
