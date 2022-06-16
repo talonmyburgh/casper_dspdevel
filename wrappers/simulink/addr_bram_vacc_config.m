74bc9625277921cb1b29f437ba642cfa61678e46
talonmyburgh <myburgh.talon@gmail.com>
2022-06-16 23:06:48 +0200

    ^^ working simulink block

--- wrappers/simulink/addr_bram_vacc_config.m
+++ wrappers/simulink/addr_bram_vacc_config.m
@@ -1,67 +1,51 @@
-
 function addr_bram_vacc_config(this_block)
 
-  filepath = fileparts(which('addr_bram_vacc_config'));
   this_block.setTopLevelLanguage('VHDL');
-  addr_bram_vacc = this_block.blockName;
-  addr_bram_vacc_parent = get_param(addr_bram_vacc,'Parent');
 
   this_block.setEntityName('addr_bram_vacc');
+  filepath = fileparts(which('addr_bram_vacc_config'));
 
-  function boolval =  checkbox2bool(bxval)
-    if strcmp(bxval, 'on')
-     boolval= true;
-    elseif strcmp(bxval, 'off')
-     boolval= false;
-    end 
-  end
-  function strboolval = bool2sign(bval)
-    if bval
-        strboolval = "SIGNED";
-    elseif ~bval
-        strboolval = "UNSIGNED";
-    end
-end
+  addr_bram_vacc_blk = this_block.blockName;
+  addr_bram_vacc_blk_parent = get_param(addr_bram_vacc_blk,'Parent');
 
-  vector_length = get_param(delay_bram_prog_parent,'vector_length');
-  vector_length_dbl = str2num(output_bit_w)
-  output_sign = checkbox2bool(get_param(delay_bram_prog_parent,'output_sign'));
-  output_sign_sign = bool2sign(output_sign)
-  output_bit_w = get_param(delay_bram_prog_parent,'output_bit_w');
-  output_bit_w_dbl = str2num(output_bit_w)
-  output_bin_pt = get_param(delay_bram_prog_parent,'output_bin_pt');
-  output_bin_pt_dbl = str2num(output_bin_pt)
+  %Extract block parameters
+  vector_length = get_param(addr_bram_vacc_blk_parent,'vector_length');
+  vector_length_dbl = str2double(vector_length);
+  addr_len = ceil(log2(vector_length_dbl));
+  is_signed = get_param(addr_bram_vacc_blk_parent,'output_sign')
+  output_bit_w = get_param(addr_bram_vacc_blk_parent,'output_bit_w');
+  output_bin_pt = get_param(addr_bram_vacc_blk_parent,'output_bin_pt');
 
   this_block.addSimulinkInport('new_acc');
   new_acc_port = this_block.port('new_acc');
+  
   this_block.addSimulinkInport('din');
-  din_port = this_block.port('din');
-
+  
   this_block.addSimulinkOutport('addr');
   addr_port = this_block.port('addr');
-  this_block.addSimulinkOutport('we');
-  we_port = this_block.port('we');
+  addr_port.setType(sprintf('Ufix_%s_0',num2str(addr_len)));
+  
   this_block.addSimulinkOutport('dout');
   dout_port = this_block.port('dout');
+  if is_signed == '"SIGNED"'
+    dout_port.setType(sprintf('Fix_%s_%s',output_bit_w,output_bin_pt))
+  elseif is_signed == '"UNSIGNED"'
+    dout_port.setType(sprintf('Ufix_%s_%s',output_bit_w,output_bin_pt))
+  end
+
+  this_block.addSimulinkOutport('we');
+  we_port = this_block.port('we');
+  we_port.setType('Ufix_1_0');
+  we_port.useHDLVector(false);
 
   % -----------------------------
   if (this_block.inputTypesKnown)
     % do input type checking, dynamic output type and generic setup in this code block.
-
-    if (this_block.port('new_acc').width ~= 1);
+    if (new_acc_port.width ~= 1)
       this_block.setError('Input data type for port "new_acc" must have width=1.');
     end
-
+    new_acc_port.setType('Ufix_1_0');
     new_acc_port.useHDLVector(false);
-    we_port.useHDLVector(false);
-
-    if(output_sign)
-      dout_port.setType(sprintf("Fix_%d_%d",output_bit_w_dbl,output_bin_pt_dbl));
-    else
-      dout_port.setType(sprintf("Ufix_%d_%d",output_bit_w_dbl,output_bin_pt_dbl));
-    end;
-
-    addr_port.setWidth(ceil(log2(vector_length_dbl)))
   end  % if(inputTypesKnown)
   % -----------------------------
 
@@ -71,34 +55,32 @@ end
    end  % if(inputRatesKnown)
   % -----------------------------
 
-    uniqueInputRates = unique(this_block.getInputRates);
-
-  % (!) Custimize the following generic settings as appropriate. If any settings depend
-  %      on input types, make the settings in the "inputTypesKnown" code block.
-  %      The addGeneric function takes  3 parameters, generic name, type and constant value.
-  %      Supported types are boolean, real, integer and string.
-  this_block.addGeneric('g_vector_length','NATURAL','16');
-  this_block.addGeneric('g_output_type','STRING','"SIGNED"');
-  this_block.addGeneric('g_bit_w','NATURAL','32');
-
-  % Add addtional source files as needed.
-  %  |-------------
-  %  | Add files in the order in which they should be compiled.
-  %  | If two files "a.vhd" and "b.vhd" contain the entities
-  %  | entity_a and entity_b, and entity_a contains a
-  %  | component of type entity_b, the correct sequence of
-  %  | addFile() calls would be:
-  %  |    this_block.addFile('b.vhd');
-  %  |    this_block.addFile('a.vhd');
-  %  |-------------
-
-  %    this_block.addFile('');
-  %    this_block.addFile('');
-  this_block.addFile('C:/Users/mybur/OneDrive/Work/CASPER/dspdevel_designs/casper_dspdevel/casper_accumulators/addr_bram_vacc.vhd');
-
+  this_block.addGeneric('g_vector_length','NATURAL',vector_length);
+  this_block.addGeneric('g_output_type','STRING',is_signed);
+  this_block.addGeneric('g_bit_w','NATURAL',output_bit_w);
+  
+  this_block.addFileToLibrary([filepath '/../../casper_accumulators/addr_bram_vacc.vhd'],'xil_defaultlib');
+  this_block.addFileToLibrary([filepath '/../../common_pkg/common_pkg.vhd'],'common_pkg_lib');
+  this_block.addFileToLibrary([filepath '/../../common_components/common_delay.vhd'],'common_components_lib');
+  this_block.addFileToLibrary([filepath '/../../misc/edge_detect.vhd'],'casper_misc_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_counter/free_run_up_counter.vhd'],'casper_counter_lib');
+  this_block.addFileToLibrary([filepath '/../../misc/pulse_ext.vhd'],'casper_misc_lib');
+  this_block.addFileToLibrary([filepath '/../../common_components/common_pipeline.vhd'],'common_components_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_adder/common_add_sub.vhd'],'casper_adder_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_ram/common_ram_pkg.vhd'],'casper_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_counter/common_counter.vhd'],'casper_counter_lib');
+  this_block.addFileToLibrary([filepath '/../../technology/technology_select_pkg.vhd'],'technology_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_ram/tech_memory_component_pkg.vhd'],'casper_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_ram/tech_memory_ram_crw_crw.vhd'],'casper_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_ram/tech_memory_ram_cr_cw.vhd'],'casper_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_ram/common_ram_crw_crw.vhd'],'casper_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_ram/common_ram_rw_rw.vhd'],'casper_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_ram/common_ram_r_w.vhd'],'casper_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../casper_delay/delay_bram.vhd'],'casper_delay_lib');
+  this_block.addFileToLibrary([filepath '/../../ip_xpm/ram/ip_xpm_ram_cr_cw.vhd'],'ip_xpm_ram_lib');
+  this_block.addFileToLibrary([filepath '/../../ip_xpm/ram/ip_xpm_ram_crw_crw.vhd'],'ip_xpm_ram_lib');
 return;
-
-
+end
 % ------------------------------------------------------------
 
 function setup_as_single_rate(block,clkname,cename) 
@@ -122,6 +104,6 @@ function setup_as_single_rate(block,clkname,cename)
   end 
   block.addClkCEPair(clkname,cename,theInputRate); 
   return; 
-
+end
 % ------------------------------------------------------------
 
