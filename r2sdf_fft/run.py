99783441f9775e5c1d0d1c7d0b18116287e83e85
Matthew Schiller <mschille@nrao.edu>
2023-03-28 19:57:44 -0400

    Fix for stack memory usage in GHDL

--- r2sdf_fft/run.py
+++ r2sdf_fft/run.py
@@ -151,6 +151,7 @@ TB_GENERATED.add_config(
 vu.set_compile_option("ghdl.a_flags", ["-frelaxed","-fsynopsys","-fexplicit","-Wno-hide"])
 #vu.set_sim_option("ghdl.elab_e", True)
 vu.set_sim_option("ghdl.elab_flags", ["-frelaxed","-fsynopsys","-fexplicit","--syn-binding"])
+vu.set_sim_option("ghdl.sim_flags",["--max-stack-alloc=4096"])
 # Don't optimize in Modelsim/Questa GUI mode
 vu.set_sim_option("modelsim.vsim_flags.gui",["-voptargs=+acc"])
 vu.main()
