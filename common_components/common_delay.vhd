572ed2add45cf4188169a50c73ed183a030c943f
Matthew Schiller <mschille@nrao.edu>
2023-06-09 13:30:54 -0400

    Fixed timing in common_delay and fixed issue with Vivado crashing

--- common_components/common_delay.vhd
+++ common_components/common_delay.vhd
@@ -28,6 +28,8 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 library common_pkg_lib;
 use common_pkg_lib.common_pkg.all;
+library casper_ram_lib;
+use casper_ram_lib.common_ram_pkg.all;
 entity common_delay is
 	generic(
 		g_dat_w : NATURAL := 8;         --! need g_dat_w to be able to use (others=>'') assignments for two dimensional unconstraint vector arrays
@@ -48,7 +50,7 @@ architecture rtl of common_delay is
 	type t_dly_arr is array (0 to g_depth) of STD_LOGIC_VECTOR(g_dat_w - 1 downto 0);
 
 	
-    
+    type t_mem_arr is array (0 to (((2**ceil_log2(g_depth))-1))) of STD_LOGIC_VECTOR(g_dat_w - 1 downto 0);
     --signal shift_reg : t_dly_arr := (others => (others => '0'));
 begin
 
@@ -58,7 +60,7 @@ begin
         shift_reg(0) <= in_dat;
 	    out_dat <= shift_reg(g_depth);
 	end generate;
-	gen_regSR : if g_depth > 0 and g_depth<140 generate -- Use a shift register implementation
+	gen_regSR : if g_depth > 0 and g_depth<128 generate -- Use a shift register implementation
         signal shift_reg : t_dly_arr := (others => (others => '0'));
     begin
     
@@ -73,52 +75,89 @@ begin
 			end if;
 		end process;
 	end generate;
-	gen_regMEM : if g_depth >= 140 generate -- Use a Memory implementation
-	--signal mem_addr_rd : unsigned(ceil_log2(g_depth-1)-1 downto 0) := to_unsigned(g_depth-2,ceil_log2(g_depth-1));
-	constant c_slice_input_regs 	: integer := 2;  -- note these are non-generic you'll need to change the code below if you change these
-	constant c_slice_output_regs 	: integer := 2;
-	constant c_mem_depth       		: integer := g_depth - (c_slice_input_regs+c_slice_output_regs) -1;
-	type t_mem_arr is array (0 to c_mem_depth-1) of STD_LOGIC_VECTOR(g_dat_w - 1 downto 0);
-	signal mem_addr_wr 				: unsigned(ceil_log2(c_mem_depth)-1 downto 0) := to_unsigned(0,ceil_log2(c_mem_depth));
-	signal mem_addr_wr_d1 			: unsigned(ceil_log2(c_mem_depth)-1 downto 0) := to_unsigned(0,ceil_log2(c_mem_depth));
-	signal mem_addr_wr_d2 			: unsigned(ceil_log2(c_mem_depth)-1 downto 0) := to_unsigned(0,ceil_log2(c_mem_depth));
-	signal input_data				: STD_LOGIC_VECTOR(g_dat_w - 1 downto 0);
-	signal input_data_d1			: STD_LOGIC_VECTOR(g_dat_w - 1 downto 0);
+	gen_regMEM : if g_depth >= 128 generate -- Use a Memory implementation
+	signal mem_addr_wr 		: unsigned(ceil_log2(g_depth-1)-1 downto 0) := to_unsigned(g_depth-5,ceil_log2(g_depth));
+	signal mem_addr_rd 		: unsigned(ceil_log2(g_depth-1)-1 downto 0) := to_unsigned(0,ceil_log2(g_depth));
+	signal out_datmem  		: STD_LOGIC_VECTOR(g_dat_w - 1 downto 0) := (others => '0'); --! Output value
+	signal out_dat_p 		: STD_LOGIC_VECTOR(g_dat_w - 1 downto 0) := (others => '0'); --! Output value
+	signal in_dat_d1		: STD_LOGIC_VECTOR(g_dat_w - 1 downto 0) := (others => '0'); --! Output value
+	signal in_dat_d2		: STD_LOGIC_VECTOR(g_dat_w - 1 downto 0) := (others => '0'); --! Output value
+	attribute DONT_TOUCH : string;
+	attribute KEEP : string;
+	attribute KEEP of out_dat_p : signal is "TRUE";  -- try to prevent this signal from being absorbed into the block
+	attribute DONT_TOUCH of out_dat_p : signal is "TRUE"; -- try to prevent this signal from being absorbed into the block
 
-	signal memory 					: t_mem_arr := (others => (others => '0'));
-    attribute ram_style: string;
-    attribute ram_style of memory: signal is "block";
-	signal out_datmem  : STD_LOGIC_VECTOR(g_dat_w - 1 downto 0) := (others => '0'); --! Output value
-	signal out_datmem_d1  : STD_LOGIC_VECTOR(g_dat_w - 1 downto 0) := (others => '0'); --! Output value
 
 
     begin
-        
-        --assert out_datmem = shift_reg(g_depth) report "non matching data" severity failure;
-		p_clk : process(clk)
+        --out_dat <= out_datmem;
+		--shift_reg(0) <= in_dat;
+		--out_shift    <= shift_reg(g_depth);
+        --assert out_datmem = shift_reg(g_depth-2) report "non matching data" severity failure;
+		--p_clk : process(clk)
+		--begin
+		--	if rising_edge(clk) then
+		--		if in_val = '1' then
+		--		    out_datmem <= memory(to_integer(mem_addr_rd));
+		--			memory(to_integer(mem_addr_wr)) <= in_dat;
+		--			--shift_reg(1 to g_depth) <= shift_reg(0 to g_depth - 1);
+		--		end if;
+		--	end if;
+		--end process;
+		RamInst : entity casper_ram_lib.tech_memory_ram_cr_cw
+			generic map(
+				g_adr_w         => ceil_log2(g_depth-1),
+				g_dat_w         => g_dat_w,
+				g_nof_words     => 2**ceil_log2(g_depth),
+				g_rd_latency    => 1
+				--g_init_file     => g_init_file,
+				--g_ram_primitive => g_ram_primitive
+			)
+			port map(
+				data      => in_dat_d2,
+				rdaddress => std_logic_vector(mem_addr_rd),
+				rdclock   => clk,
+				rdclocken => in_val,
+				wraddress => std_logic_vector(mem_addr_wr),
+				wrclock   => clk,
+				wrclocken => in_val,
+				wren      => in_val,
+				q         => out_datmem
+			);
+		
+		--RamInst : entity casper_ram_lib.common_ram_r_w
+		--	generic map(
+		--		g_ram            => c_mem_settings,
+		--		--g_init_file      => g_init_file,
+		--		g_true_dual_port => false
+		--		--g_ram_primitive  => g_ram_primitive
+		--	)
+		--	port map(
+		--		clk    => clk,
+		--		clken  => in_val,
+		--		wr_en  => in_val,
+		--		wr_adr => std_logic_vector(mem_addr_wr),
+		--		wr_dat => in_dat,
+		--		rd_en  => in_val,
+		--		rd_adr => std_logic_vector(mem_addr_rd),
+		--		rd_dat => out_datmem,
+		--		rd_val => open
+		--	);
+		
+		reg_data : process (clk)
 		begin
 			if rising_edge(clk) then
-
-				if in_val = '1' then
-					mem_addr_wr_d1 	<= mem_addr_wr;
-					mem_addr_wr_d2 	<= mem_addr_wr_d1;
-
-				    out_datmem 		<= memory(to_integer(mem_addr_wr_d2));
-					out_datmem_d1   <= out_datmem;
-					out_dat 		<= out_datmem_d1;
-					input_data		<= in_dat;
-					input_data_d1	<= input_data;
-					memory(to_integer(mem_addr_wr_d2)) <= input_data_d1;
-					--mem_addr_rd <= mem_addr_wr;
-					if mem_addr_wr=(c_mem_depth-1) then
-					   mem_addr_wr 	<= to_unsigned(0,mem_addr_wr'length);
-					else
-					   mem_addr_wr 	<= mem_addr_wr + 1;
-					end if;
-
-					--shift_reg(1 to g_depth) <= shift_reg(0 to g_depth - 1);
+				if in_val='1' then
+					in_dat_d1 <= in_dat;
+					in_dat_d2 <= in_dat_d1;
+					out_dat_p <= out_datmem;
+					out_dat <= out_dat_p;
+					mem_addr_wr <= mem_addr_wr + 1;
+					mem_addr_rd <= mem_addr_rd + 1;
 				end if;
 			end if;
 		end process;
+
+					
 	end generate;
 end rtl;
