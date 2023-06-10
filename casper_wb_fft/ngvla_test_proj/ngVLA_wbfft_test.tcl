b93327c79657b68e3301f13890abae2c3f7a3353
Matthew Schiller <mschille@nrao.edu>
2023-06-10 10:24:50 -0400

    Add vunit to place and route

--- casper_wb_fft/ngvla_test_proj/ngVLA_wbfft_test.tcl
+++ casper_wb_fft/ngvla_test_proj/ngVLA_wbfft_test.tcl
@@ -1,5 +1,11 @@
+package require ::tclapp::xilinx::junit
+namespace import ::tclapp::xilinx::junit::*
+# See: https://github.com/Xilinx/XilinxTclStore/blob/master/tclapp/xilinx/junit/README.md
+
 set origin_dir "."
 set outputDir ./synth_output
+set_report ./report.xml
+
 file mkdir $outputDir
 
 
@@ -74,5 +80,46 @@ read_vhdl -library ip_xpm_ram_lib "$origin_dir/../../ip_xpm/ram/ip_xpm_ram_crw_c
 read_vhdl -library ip_xpm_ram_lib "$origin_dir/../../ip_xpm/ram/ip_xpm_rom_r.vhd"
 read_vhdl -library ip_xpm_ram_lib "$origin_dir/../../ip_xpm/ram/ip_xpm_rom_r_r.vhd"
 
-synth_design -top top -part xczu48dr-ffvg1517-1-i
+update_compile_order
+
+run_step {synth_design -top top -part xczu48dr-ffvg1517-1-i}
+
 write_checkpoint -force $outputDir/post_synth
+# validation
+validate_timing "Post synth_design"
+validate_logic "Post synth_design"
+
+
+# opt_design
+#run_step {opt_design}
+#write_checkpoint opt_design.dcp
+# validation
+#validate_timing "Post opt_design"
+#validate_logic "Post opt_design"
+
+# place_design
+#run_step {place_design}
+#write_checkpoint place_design.dcp
+# validation
+#validate_timing "Post place_design"
+#validate_logic "Post place_design"
+
+# phys_opt_design
+#run_step {phys_opt_design}
+#write_checkpoint phys_opt_design.dcp
+# validation
+#validate_timing "Post phys_opt_design"
+#validate_logic "Post phys_opt_design"
+
+# route_design
+#run_step {route_design}
+#write_checkpoint route_design.dcp
+# validation
+#validate_timing "Post route_design"
+#validate_routing "Post route_design"
+
+
+validate_messages "Final"
+validate_drcs "Final"
+
+write_results
