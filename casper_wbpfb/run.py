c8fd401dc7f0207419e0f1f62e138372248dfdd1
Matthew Schiller <mschille@nrao.edu>
2023-07-12 08:54:47 -0400

    Update file run.py

--- casper_wbpfb/run.py
+++ casper_wbpfb/run.py
@@ -19,7 +19,7 @@ def manglePkg(file_name, line_number, new_line):
 vu = VUnit.from_argv()
 vu.add_vhdl_builtins()
 # XPM Library compile
-lib_xpm = vu.add_library("xpm","2002")
+lib_xpm = vu.add_library("xpm")
 lib_xpm.add_source_files(join(script_dir, "../xilinx/xpm_vhdl/src/xpm/xpm_VCOMP.vhd"))
 xpm_source_file_base = lib_xpm.add_source_file(join(script_dir, "../xilinx/xpm_vhdl/src/xpm/xpm_memory/hdl/xpm_memory_base.vhd"))
 xpm_source_file_sdpram = lib_xpm.add_source_file(join(script_dir, "../xilinx/xpm_vhdl/src/xpm/xpm_memory/hdl/xpm_memory_sdpram.vhd"))
@@ -32,25 +32,25 @@ xpm_source_file_tdpram.add_dependency_on(xpm_source_file_base)
 xpm_source_file_dprom.add_dependency_on(xpm_source_file_base)
 
 # Altera_mf library
-lib_altera_mf = vu.add_library("altera_mf","2002")
+lib_altera_mf = vu.add_library("altera_mf")
 lib_altera_mf.add_source_file(join(script_dir, "../intel/altera_mf/altera_mf_components.vhd"))
 altera_mf_source_file = lib_altera_mf.add_source_file(join(script_dir, "../intel/altera_mf/altera_mf.vhd"))
 
 # XPM Multiplier library
-ip_xpm_mult_lib = vu.add_library("ip_xpm_mult_lib","2002", allow_duplicate=True)
+ip_xpm_mult_lib = vu.add_library("ip_xpm_mult_lib", allow_duplicate=True)
 ip_cmult_3dsp = ip_xpm_mult_lib.add_source_file(join(script_dir, "../ip_xpm/mult/ip_cmult_rtl_3dsp.vhd"))
 ip_cmult_4dsp = ip_xpm_mult_lib.add_source_file(join(script_dir, "../ip_xpm/mult/ip_cmult_rtl_4dsp.vhd"))
 ip_mult_infer = ip_xpm_mult_lib.add_source_file(join(script_dir, "../ip_xpm/mult/ip_mult_infer.vhd"))
 
 # STRATIXIV Multiplier library
-ip_stratixiv_mult_lib = vu.add_library("ip_stratixiv_mult_lib","2002", allow_duplicate=True)
+ip_stratixiv_mult_lib = vu.add_library("ip_stratixiv_mult_lib", allow_duplicate=True)
 ip_stratixiv_complex_mult_rtl = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_complex_mult_rtl.vhd"))
 ip_stratixiv_complex_mult = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_complex_mult.vhd"))
 ip_stratixiv_mult_rtl = ip_stratixiv_mult_lib.add_source_file(join(script_dir, "../ip_stratixiv/mult/ip_stratixiv_mult_rtl.vhd"))
 ip_stratixiv_complex_mult.add_dependency_on(altera_mf_source_file)
 
 # XPM RAM library
-ip_xpm_ram_lib = vu.add_library("ip_xpm_ram_lib","2002")
+ip_xpm_ram_lib = vu.add_library("ip_xpm_ram_lib")
 ip_xpm_file_cr_cw = ip_xpm_ram_lib.add_source_files(join(script_dir, "../ip_xpm/ram/ip_xpm_ram_cr_cw.vhd"))
 ip_xpm_file_cr_cw.add_dependency_on(xpm_source_file_sdpram)
 ip_xpm_file_crw_crw = ip_xpm_ram_lib.add_source_files(join(script_dir, "../ip_xpm/ram/ip_xpm_ram_crw_crw.vhd"))
@@ -59,14 +59,14 @@ ip_xpm_file_crw_crw = ip_xpm_ram_lib.add_source_files(join(script_dir, "../ip_xp
 ip_xpm_file_crw_crw.add_dependency_on(xpm_source_file_tdpram)
 
 # STRATIXIV RAM Library
-ip_stratixiv_ram_lib = vu.add_library("ip_stratixiv_ram_lib","2002")
+ip_stratixiv_ram_lib = vu.add_library("ip_stratixiv_ram_lib")
 ip_stratix_file_cr_cw = ip_stratixiv_ram_lib.add_source_file(join(script_dir, "../ip_stratixiv/ram/ip_stratixiv_ram_cr_cw.vhd"))
 ip_stratix_file_crw_crw = ip_stratixiv_ram_lib.add_source_file(join(script_dir, "../ip_stratixiv/ram/ip_stratixiv_ram_crw_crw.vhd"))
 ip_stratix_file_cr_cw.add_dependency_on(altera_mf_source_file)
 ip_stratix_file_crw_crw.add_dependency_on(altera_mf_source_file)
 
 # COMMON COMPONENTS Library
-common_components_lib = vu.add_library("common_components_lib","2002")
+common_components_lib = vu.add_library("common_components_lib")
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_pipeline.vhd"))
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_bit_delay.vhd"))
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_pipeline_sl.vhd"))
@@ -77,51 +77,51 @@ common_components_lib.add_source_files(join(script_dir, "../common_components/co
 common_components_lib.add_source_files(join(script_dir, "../common_components/common_components_pkg.vhd"))
 
 # COMMON PACKAGE Library
-common_pkg_lib = vu.add_library("common_pkg_lib","2002")
+common_pkg_lib = vu.add_library("common_pkg_lib")
 common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_float_types_c.vhd"))
-#if SIMULATOR_FACTORY.select_simulator().name == "ghdl":
-#    #common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_float_types_c_2008redirect.vhdl"))
-#    #common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_generic_pkg-body_2008redirect.vhdl"))
-#    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_generic_pkg_2008redirect.vhdl"))
-#    #common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_generic_pkg-body_2008redirect.vhdl"))
-#    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_generic_pkg_2008redirect.vhdl"))
-#    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_pkg_c_2008redirect.vhdl"))
-#    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_pkg_c_2008redirect.vhdl"))
-#else:
+if SIMULATOR_FACTORY.select_simulator().name == "ghdl":
+    #common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_float_types_c_2008redirect.vhdl"))
+    #common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_generic_pkg-body_2008redirect.vhdl"))
+    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_generic_pkg_2008redirect.vhdl"))
+    #common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_generic_pkg-body_2008redirect.vhdl"))
+    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_generic_pkg_2008redirect.vhdl"))
+    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_pkg_c_2008redirect.vhdl"))
+    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_pkg_c_2008redirect.vhdl"))
+else:
     # use the "hacked" up VHDL93 version in other simulators 
-common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_pkg_c.vhd"))
-common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_pkg_c.vhd"))
+    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/fixed_pkg_c.vhd"))
+    common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/float_pkg_c.vhd"))
 common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/common_pkg.vhd"))
 common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/common_str_pkg.vhd"))
 common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/tb_common_pkg.vhd"))
 common_pkg_lib.add_source_files(join(script_dir, "../common_pkg/common_lfsr_sequences_pkg.vhd"))
 
 # DP PACKAGE Library
-dp_pkg_lib = vu.add_library("dp_pkg_lib","2002")
+dp_pkg_lib = vu.add_library("dp_pkg_lib")
 dp_pkg_lib.add_source_file(join(script_dir, "../casper_dp_pkg/dp_stream_pkg.vhd"))
 
 # DP COMPONENTS Library
-dp_components_lib = vu.add_library("dp_components_lib","2002")
+dp_components_lib = vu.add_library("dp_components_lib")
 dp_components_lib.add_source_file(join(script_dir, "../casper_dp_components/dp_hold_input.vhd"))
 dp_components_lib.add_source_file(join(script_dir, "../casper_dp_components/dp_hold_ctrl.vhd"))
 dp_components_lib.add_source_file(join(script_dir, "../casper_dp_components/dp_block_gen.vhd"))
 
 # TECHNOLOGY Library
-technology_lib = vu.add_library("technology_lib","2002")
+technology_lib = vu.add_library("technology_lib")
 technology_lib.add_source_files(join(script_dir, "../technology/technology_select_pkg.vhd"))
 
 # COMMON COUNTER Library
-casper_counter_lib = vu.add_library("casper_counter_lib","2002")
+casper_counter_lib = vu.add_library("casper_counter_lib")
 casper_counter_lib.add_source_file(join(script_dir, "../casper_counter/common_counter.vhd"))
 
 # CASPER ADDER Library
-casper_adder_lib = vu.add_library("casper_adder_lib","2002")
+casper_adder_lib = vu.add_library("casper_adder_lib")
 casper_adder_lib.add_source_file(join(script_dir, "../casper_adder/common_add_sub.vhd"))
 casper_adder_lib.add_source_file(join(script_dir, "../casper_adder/common_adder_tree.vhd"))
 casper_adder_lib.add_source_file(join(script_dir, "../casper_adder/common_adder_tree_a_str.vhd"))
 
 # CASPER MUlTIPLIER Library
-casper_multiplier_lib = vu.add_library("casper_multiplier_lib","2002")
+casper_multiplier_lib = vu.add_library("casper_multiplier_lib")
 casper_multiplier_lib.add_source_file(join(script_dir, "../casper_multiplier/tech_mult_component.vhd"))
 tech_complex_mult = casper_multiplier_lib.add_source_file(join(script_dir, "../casper_multiplier/tech_complex_mult.vhd"))
 casper_multiplier_lib.add_source_file(join(script_dir, "../casper_multiplier/tech_agilex_versal_cmult.vhd"))
@@ -135,20 +135,20 @@ tech_complex_mult.add_dependency_on(ip_stratixiv_complex_mult)
 tech_complex_mult.add_dependency_on(ip_stratixiv_complex_mult_rtl)
 
 # CASPER MULTIPLEXER Library
-casper_multiplexer_lib = vu.add_library("casper_multiplexer_lib","2002")
+casper_multiplexer_lib = vu.add_library("casper_multiplexer_lib")
 casper_multiplexer_lib.add_source_files(join(script_dir, "../casper_multiplexer/common_multiplexer.vhd"))
 casper_multiplexer_lib.add_source_files(join(script_dir, "../casper_multiplexer/common_zip.vhd"))
 casper_multiplexer_lib.add_source_files(join(script_dir, "../casper_multiplexer/common_demultiplexer.vhd"))
 
 # CASPER REQUANTIZE Library
-casper_requantize_lib = vu.add_library("casper_requantize_lib","2002")
+casper_requantize_lib = vu.add_library("casper_requantize_lib")
 casper_requantize_lib.add_source_file(join(script_dir, "../casper_requantize/common_round.vhd"))
 casper_requantize_lib.add_source_file(join(script_dir, "../casper_requantize/common_resize.vhd"))
 casper_requantize_lib.add_source_file(join(script_dir, "../casper_requantize/common_requantize.vhd"))
 casper_requantize_lib.add_source_file(join(script_dir, "../casper_requantize/r_shift_requantize.vhd"))
 
 # CASPER RAM Library
-casper_ram_lib = vu.add_library("casper_ram_lib","2002")
+casper_ram_lib = vu.add_library("casper_ram_lib")
 casper_ram_lib.add_source_file(join(script_dir, "../casper_ram/common_ram_pkg.vhd"))
 casper_ram_lib.add_source_file(join(script_dir, "../casper_ram/tech_memory_component_pkg.vhd"))
 casper_ram_lib.add_source_file(join(script_dir, "../casper_ram/tech_memory_ram_crw_crw.vhd"))
@@ -171,7 +171,7 @@ fil_pkg = join(script_dir, "../casper_filter/fil_pkg.vhd")
 fil_line_entries = ['CONSTANT c_fil_in_dat_w       : natural := 8;\n','CONSTANT c_fil_out_dat_w      : natural := 16;\n','CONSTANT c_fil_coef_dat_w    : natural := 16;\n']
 manglePkg(fil_pkg, slice(7,10), fil_line_entries)
 
-casper_filter_lib = vu.add_library("casper_filter_lib","2002")
+casper_filter_lib = vu.add_library("casper_filter_lib")
 casper_filter_lib.add_source_file(fil_pkg)
 casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_ctrl.vhd"))
 casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_filter.vhd"))
@@ -179,23 +179,23 @@ casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_sin
 casper_filter_lib.add_source_file(join(script_dir, "../casper_filter/fil_ppf_wide.vhd"))
 
 # CASPER MEM Library
-casper_mm_lib = vu.add_library("casper_mm_lib","2002")
+casper_mm_lib = vu.add_library("casper_mm_lib")
 casper_mm_lib.add_source_file(join(script_dir, "../casper_mm/tb_common_mem_pkg.vhd"))
 
 # CASPER SIM TOOLS Library
-casper_sim_tools_lib = vu.add_library("casper_sim_tools_lib","2002")
+casper_sim_tools_lib = vu.add_library("casper_sim_tools_lib")
 casper_sim_tools_lib.add_source_file(join(script_dir, "../casper_sim_tools/common_wideband_data_scope.vhd"))
 
 # CASPER PIPELINE Library
-casper_pipeline_lib = vu.add_library("casper_pipeline_lib","2002")
+casper_pipeline_lib = vu.add_library("casper_pipeline_lib")
 casper_pipeline_lib.add_source_file(join(script_dir, "../casper_pipeline/dp_pipeline.vhd"))
 
 # CASPER DIAGNOSTICS Library
-casper_diagnostics_lib = vu.add_library("casper_diagnostics_lib","2002")
+casper_diagnostics_lib = vu.add_library("casper_diagnostics_lib")
 casper_diagnostics_lib.add_source_file(join(script_dir, "../casper_diagnostics/diag_pkg.vhd"))
 
 # RTWOSDF Library
-r2sdf_fft_lib = vu.add_library("r2sdf_fft_lib","2002")
+r2sdf_fft_lib = vu.add_library("r2sdf_fft_lib")
 r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoBF.vhd"))
 r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoBFStage.vhd"))
 r2sdf_fft_lib.add_source_file(join(script_dir, "../r2sdf_fft/rTwoOrder.vhd"))
@@ -213,7 +213,7 @@ fft_pkg = join(script_dir, "../casper_wb_fft/fft_gnrcs_intrfcs_pkg.vhd")
 fft_line_entries = ['CONSTANT c_fft_in_dat_w       : natural := 16;\n','CONSTANT c_fft_out_dat_w      : natural := 16;\n','CONSTANT c_fft_stage_dat_w    : natural := 18;\n']
 manglePkg(fft_pkg, slice(7,10), fft_line_entries)
 
-wb_fft_lib = vu.add_library("wb_fft_lib","2002")
+wb_fft_lib = vu.add_library("wb_fft_lib")
 wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_sepa.vhd"))
 wb_fft_lib.add_source_file(fft_pkg)
 wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/tb_fft_pkg.vhd"))
@@ -225,7 +225,7 @@ wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_r2_wide.vhd"))
 wb_fft_lib.add_source_file(join(script_dir, "../casper_wb_fft/fft_sepa_wide.vhd"))
 
 # WIDEBAND PFB Library
-wpfb_lib = vu.add_library("wpfb_lib","2002")
+wpfb_lib = vu.add_library("wpfb_lib")
 wpfb_lib.add_source_file(join(script_dir, "wbpfb_gnrcs_intrfcs_pkg.vhd"))
 wpfb_lib.add_source_file(join(script_dir, "dp_bsn_restore_global.vhd"))
 wpfb_lib.add_source_file(join(script_dir, "dp_block_gen_valid_arr.vhd"))
