7b99a2f119402069323f609f32716bd5176cf4d6
talonmyburgh <myburgh.talon@gmail.com>
2023-11-10 21:22:28 +0200

    ** remove rst from wbpfb block - may not work still since option for xtra data signals still exists even though they are not available

--- wrappers/simulink/wb_fft_subsys2bb_knit.m
+++ wrappers/simulink/wb_fft_subsys2bb_knit.m
@@ -1,9 +1,13 @@
+%--------------------------------------------------------------------------------
+%- Created for use in the CASPER ecosystem by Talon Myburgh under Mydon Solutions
+%- myburgh.talon@gmail.com
+%- https://github.com/talonmyburgh | https://github.com/MydonSolutions
+%--------------------------------------------------------------------------------%
 function wb_fft_subsys2bb_knit()
     %fetch wideband parameters to figure out which ports to draw
     subsysblk = gcb;
     wb_fft_bb = [subsysblk '/wb_fft'];
     wb_factor = str2double(get_param(subsysblk,'wb_factor'));
-    xtra_dat_sigs = checkbox2bool(get_param(subsysblk,'xtra_dat_sigs'));
     
     function boolval =  checkbox2bool(bxval)
        if strcmp(bxval, 'on')
@@ -29,25 +33,6 @@ function wb_fft_subsys2bb_knit()
         in_im = find(contains(inprts,'in_im_'));
         out_re = find(contains(outprts,'out_re_'));
         out_im = find(contains(outprts,'out_im_'));
-    end
-
-    function updateotherprts()
-        subsysblk = gcb;
-        inprts = find_system(subsysblk,'LookUnderMasks','on','BlockType','Inport');
-        outprts = find_system(subsysblk,'LookUnderMasks','on','BlockType','Outport');
-        in_bsn = find(contains(inprts,'in_bsn'));
-        in_sop = find(contains(inprts,'in_sop'));
-        in_eop = find(contains(inprts,'in_eop'));
-        in_empty = find(contains(inprts,'in_empty'));
-        in_err = find(contains(inprts,'in_err'));
-        in_channel = find(contains(inprts,'in_channel'));
-
-        out_bsn = find(contains(outprts,'out_bsn'));
-        out_sop = find(contains(outprts,'out_sop'));
-        out_eop = find(contains(outprts,'out_eop'));
-        out_empty = find(contains(outprts,'out_empty'));
-        out_err = find(contains(outprts,'out_err'));
-        out_channel = find(contains(outprts,'out_channel'));
     end
     
     % Unlink block, otherwise we're not allowed to modify it
@@ -91,153 +76,6 @@ function wb_fft_subsys2bb_knit()
             delete_line(outprt_re_lh.Inport(1));
             delete_block(outport_re{1});
         end
-    end
-    
-    updateotherprts();
-    
-    if(xtra_dat_sigs && (isempty(in_bsn) == 0))
-        %do nothing
-        
-    %In this case, we don't want extra data signals but they are present 
-    %in the design   
-    elseif(~xtra_dat_sigs && (isempty(in_bsn) ==0))
-        %delete data sigs
-        deleteDatPrts();
-        
-        %delete signals 
-        i_bsn = inprts(in_bsn);
-        in_bsn_lh = get_param(i_bsn{1},'LineHandles');
-        delete_line(in_bsn_lh.Outport);
-        delete_block(i_bsn{1});
-        
-        i_sop = inprts(in_sop);
-        in_sop_lh = get_param(i_sop{1},'LineHandles');
-        delete_line(in_sop_lh.Outport);
-        delete_block(i_sop{1});
-        
-        i_eop = inprts(in_eop);
-        in_eop_lh = get_param(i_eop{1},'LineHandles');
-        delete_line(in_eop_lh.Outport);
-        delete_block(i_eop{1});
-        
-        i_empty = inprts(in_empty);
-        in_empty_lh = get_param(i_empty{1},'LineHandles');
-        delete_line(in_empty_lh.Outport);
-        delete_block(i_empty{1});
-        
-        i_err = inprts(in_err);
-        in_err_lh = get_param(i_err{1},'LineHandles');
-        delete_line(in_err_lh.Outport);
-        delete_block(i_err{1});
-        
-        i_chan = inprts(in_channel);
-        in_chan_lh = get_param(i_chan{1},'LineHandles');
-        delete_line(in_chan_lh.Outport);
-        delete_block(i_chan{1});
-        
-        o_bsn = outprts(out_bsn);
-        out_bsn_lh = get_param(o_bsn{1},'LineHandles');
-        delete_line(out_bsn_lh.Inport);
-        delete_block(o_bsn{1});
-        
-        o_sop = outprts(out_sop);
-        out_sop_lh = get_param(o_sop{1},'LineHandles');
-        delete_line(out_sop_lh.Inport);
-        delete_block(o_sop{1});
-        
-        o_eop = outprts(out_eop);
-        out_eop_lh = get_param(o_eop{1},'LineHandles');
-        delete_line(out_eop_lh.Inport);
-        delete_block(o_eop{1});
-        
-        o_empty = outprts(out_empty);
-        out_empty_lh = get_param(o_empty{1},'LineHandles');
-        delete_line(out_empty_lh.Inport);
-        delete_block(o_empty{1});
-        
-        o_err = outprts(out_err);
-        out_err_lh = get_param(o_err{1},'LineHandles');
-        delete_line(out_err_lh.Inport);
-        delete_block(o_err{1});
-        
-        o_chan = outprts(out_channel);
-        out_chan_lh = get_param(o_chan{1},'LineHandles');
-        delete_line(out_chan_lh.Inport);
-        delete_block(o_chan{1});
-        
-    %In this case, we want extra signals but they are not present in the
-    %design
-    elseif(xtra_dat_sigs && (isempty(in_bsn) == 1))
-        bbports=get_param(wb_fft_bb,'PortHandles');
-        %Delete ports so Simulink cannot complain.
-        deleteDatPrts();
-        
-        %create signals
-        %in ports
-        in_bsn_str = sprintf([subsysblk '/in_bsn']);
-        add_block('simulink/Commonly Used Blocks/In1',in_bsn_str);
-        in_bsn_ph = get_param(in_bsn_str,'PortHandles');
-        add_line(subsysblk,in_bsn_ph.Outport(1),bbports.Inport(5));
-               
-        in_sop_str = sprintf([subsysblk '/in_sop']);
-        add_block('simulink/Commonly Used Blocks/In1',in_sop_str);
-        in_sop_ph = get_param(in_sop_str,'PortHandles');
-        add_line(subsysblk,in_sop_ph.Outport(1),bbports.Inport(6));
-        
-        in_eop_str = sprintf([subsysblk '/in_eop']);
-        add_block('simulink/Commonly Used Blocks/In1',in_eop_str);
-        in_eop_ph = get_param(in_eop_str,'PortHandles');
-        add_line(subsysblk,in_eop_ph.Outport(1),bbports.Inport(7));
-        
-        in_empty_str = sprintf([subsysblk '/in_empty']);
-        add_block('simulink/Commonly Used Blocks/In1',in_empty_str);
-        in_empty_ph = get_param(in_empty_str,'PortHandles');
-        add_line(subsysblk,in_empty_ph.Outport(1),bbports.Inport(8));
-        
-        in_err_str = sprintf([subsysblk '/in_err']);
-        add_block('simulink/Commonly Used Blocks/In1',in_err_str);
-        in_err_ph = get_param(in_err_str,'PortHandles');
-        add_line(subsysblk,in_err_ph.Outport(1),bbports.Inport(9));        
-        
-        in_chan_str = sprintf([subsysblk '/in_channel']);
-        add_block('simulink/Commonly Used Blocks/In1',in_chan_str);
-        in_chan_ph = get_param(in_chan_str,'PortHandles');
-        add_line(subsysblk,in_chan_ph.Outport(1),bbports.Inport(10));
-        
-        %out ports
-        out_bsn_str = sprintf([subsysblk '/out_bsn']);
-        add_block('simulink/Commonly Used Blocks/Out1',out_bsn_str);
-        out_bsn_ph = get_param(out_bsn_str,'PortHandles');
-        add_line(subsysblk,bbports.Outport(4),out_bsn_ph.Inport(1));
-        
-        out_sop_str = sprintf([subsysblk '/out_sop']);
-        add_block('simulink/Commonly Used Blocks/Out1',out_sop_str);
-        out_sop_ph = get_param(out_sop_str,'PortHandles');
-        add_line(subsysblk,bbports.Outport(5),out_sop_ph.Inport(1));
-
-        out_eop_str = sprintf([subsysblk '/out_eop']);
-        add_block('simulink/Commonly Used Blocks/Out1',out_eop_str);
-        out_eop_ph = get_param(out_eop_str,'PortHandles');
-        add_line(subsysblk,bbports.Outport(6),out_eop_ph.Inport(1));
-        
-        out_empty_str = sprintf([subsysblk '/out_empty']);
-        add_block('simulink/Commonly Used Blocks/Out1',out_empty_str);
-        out_empty_ph = get_param(out_empty_str,'PortHandles');
-        add_line(subsysblk,bbports.Outport(7),out_empty_ph.Inport(1));
-        
-        out_err_str = sprintf([subsysblk '/out_err']);
-        add_block('simulink/Commonly Used Blocks/Out1',out_err_str);
-        out_err_ph = get_param(out_err_str,'PortHandles');
-        add_line(subsysblk,bbports.Outport(8),out_err_ph.Inport(1)); 
-        
-        out_chan_str = sprintf([subsysblk '/out_channel']);
-        add_block('simulink/Commonly Used Blocks/Out1',out_chan_str);
-        out_chan_ph = get_param(out_chan_str,'PortHandles');
-        add_line(subsysblk,bbports.Outport(9),out_chan_ph.Inport(1));  
-    
-    %In this case, we don't want extra signals and they are are not present    
-    else
-        %do nothing
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -291,18 +129,14 @@ function wb_fft_subsys2bb_knit()
     else
         %Get bb port handles to add signals
         bbports=get_param(wb_fft_bb,'PortHandles');
-        if xtra_dat_sigs
-            i_basic_prts = 10;
-            o_basic_prts = 10;
-        else
-            i_basic_prts = 4;
-            o_basic_prts = 4;
-        end
+
+        i_basic_prts = 4;
+        o_basic_prts = 4;
+
         %start index for ports on bb
         i_p_h_i = i_basic_prts + 2*curr_prts;
         o_p_h_i = o_basic_prts + 2*curr_prts;
 
- 
         %create necessary in/out prts and add lines:
         for i=curr_prts:wb_factor-1
            %add inports/outports and set names to match bb
